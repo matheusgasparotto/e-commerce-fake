01d81c158e57954ceed072dee7c5cd8ac3e7cf99
matheusgandrade <matheus.gandrade2018@gmail.com>
2020-12-01 19:16:46 -0300

    feat(add and remove cart): create a function to add and remove itens

---
+++ src/Routers/style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { AppBar } from "@material-ui/core";
+
+export const StyledAppBar = styled(AppBar)`
+  display: flex;
+  flex-direction: row;
+  height: 10vh;
+  button {
+    width: 100px;
+    color: white;
+  }
+`;
